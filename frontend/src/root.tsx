484b63026d68a9df5f71e86d3bf3e217b133e489
sivolcy <sivolcy@foxmail.com>
2025-11-02 02:17:39 +0800

    sivolcy首次提交，将代码中的Openhands改成CompareGPT,同时替换了title、logo和图标等

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -31,8 +31,8 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export const meta: MetaFunction = () => [
-  { title: "OpenHands" },
-  { name: "description", content: "Let's Start Building!" },
+  { title: "CompareGPT" },
+  { make: "description", content: "Let's Start Building!" },
 ];
 
 export default function App() {
