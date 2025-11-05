2eef6924d882d7d9d018d9b48596c4b119ee8880
sivolcy <sivolcy@foxmail.com>
2025-11-05 04:43:14 +0000

    Resolve conflicts

---
+++ gen_token.py
@@ -0,0 +1,10 @@
+#1、服务器利用下列代码加密生成token（JWT）---->2、（JWT）客户端----->3、服务器收到jwt解密后--->4、解密后jwt_secret与config.toml中的jwt_secret对比--->相同/不相同
+#cgpt_OznsUz9UrgbTwnsnMNsOjCVCKSfvo9OU
+#-----------------------------------------------------------------开发环境------------------------------------------------------------------------------
+import jwt
+payload={'user_id': 1, 'user_name': 'platformaigpt@gmail.com', 'role': 'subscription', 'api_key': 'cgpt_mI6JLGA9sblPvrpgjz20BMsMOnm4uxqC', 'exp': 1771320280}
+print( jwt.encode(payload, "vgb0tnl9d58+6n-6h-ea&u^1#s0ccp!794=kbvqacjq75vzps$", algorithm="HS256") )
+#-----------------------------------------------------------------生产环境------------------------------------------------------------------------------
+# import jwt
+# payload={'user_id': 86, 'user_name': 'sivolcy', 'role': 'subscription', 'api_key': 'cgpt_ZeL49OeTcd5hJNqT0YkqdvuL5POPsv4f', 'exp': 1771320280}
+# print( jwt.encode(payload, "vgb0tnl9d58+6n-6h-ea&u^1#s0ccp!794=kbvqacjq75vzps$", algorithm="HS256") )
